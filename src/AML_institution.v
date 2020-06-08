ccfb0d790f71d5869700cd8f8967f6d85b5b0692
Jan Tusil <jenda.tusil@gmail.com>
2020-06-08 11:44:40 +0200

    Signatures are category (in progress)

--- src/AML_institution.v
+++ src/AML_institution.v
@@ -1,6 +1,7 @@
 (*Require Import Coq.Program.Basics.*)
 Require Import Lib.AML.
 Require Import Categories.Essentials.Notations.
+Require Import Categories.Category.Main.
 
 Print Signature.
 Record SignatureMorphism{from to : Signature} : Type :=
@@ -20,4 +21,24 @@ Definition SignatureMorphism_compose{A B C : Signature}(g: @SignatureMorphism B 
   {| evarsMorphism := fun x : evars A => evarsMorphism g (evarsMorphism f x);
      svarsMorphism := fun x : svars A => svarsMorphism g (svarsMorphism f x);
      symbolsMorphism := fun x => symbolsMorphism g (symbolsMorphism f x);
+  |}.
+
+Definition SignatureMorphism_compose_assoc :
+  forall (A B C D : Signature)
+         (f: @SignatureMorphism A B)
+         (g: @SignatureMorphism B C)
+         (h: @SignatureMorphism C D),
+    SignatureMorphism_compose h (SignatureMorphism_compose g f)
+    = SignatureMorphism_compose (SignatureMorphism_compose h g) f.
+Proof.
+  intros. auto.
+Qed.
+
+Definition Signature_Cat : Category :=
+  {| Obj := Signature;
+     Hom := fun A B => @SignatureMorphism A B;
+     compose := fun _ _ _ f g => SignatureMorphism_compose g f;
+     assoc := SignatureMorphism_compose_assoc;
+     assoc_sym := fun A B C D f g h => eq_sym (SignatureMorphism_compose_assoc A B C D f g h);
+     id := @SignatureMorphism_id;
   |}.
