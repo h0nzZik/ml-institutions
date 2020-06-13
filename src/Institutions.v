bcedaf3dd67f3dfc769d220f16fbc6aa2bf42542
Jan Tusil <jenda.tusil@gmail.com>
2020-06-13 22:12:49 +0200

    InstitutionMorphism (almost)

--- src/Institutions.v
+++ src/Institutions.v
@@ -3,6 +3,7 @@ Require Import Categories.Category.Main.
 Require Import Categories.Cat.Cat.
 Require Import Categories.Coq_Cats.Main.
 Require Import Categories.Functor.Main.
+Require Import Categories.NatTrans.Main.
 
 Class Institution : Type :=
   { Sign : Category;
@@ -12,7 +13,18 @@ Class Institution : Type :=
     (* Sign-indexed relation on objects of (Mod s) x (Sen s) *)
     satisfies: forall s:Sign, (FO Mod s) -> (FO Sen s) -> Prop;
 
-    satisfies_cond : forall (s1 s2 : Sign) (phi : Hom s1 s2) (m' : FO Mod s2) (f : FO Sen s1),
+    inst_satisfies_cond : forall (s1 s2 : Sign) (phi : Hom s1 s2) (m' : FO Mod s2) (f : FO Sen s1),
         satisfies s2 m' ((FA Sen phi) f) <-> satisfies s1 (FO (FA Mod phi) m') f;
   }.
 
+Check Functor_compose.
+Record InstitutionMorphism (I1 I2 : Institution) :=
+  { sign_transform : Functor (@Sign I1) (@Sign I2);
+    mod_transform : NatTrans (@Mod I1) (Functor_compose sign_transform (@Mod I2));
+    sen_transform : NatTrans (Functor_compose sign_transform  (@Sen I2)) (@Sen I1);
+    instmorph_satisf_condition :
+      forall (sigma : @Sign I1)
+             (m : (FO (@Mod I1)) sigma)
+             (f' : (FO (@Sen I2)) (FO sign_transform sigma)),
+        True;
+  }.
